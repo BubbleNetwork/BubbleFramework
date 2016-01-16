9fef253728e8d0185f4850e4c77b704f5cf4a4d0
ExtendObject <clipcojnh@gmail.com>
2016-01-16 15:33:32 +0000

    Updating waiting and pregame stages
    Set proper waiting time and interval

--- src/com/thebubblenetwork/api/game/BubbleGameAPI.java
+++ src/com/thebubblenetwork/api/game/BubbleGameAPI.java
@@ -153,11 +153,11 @@ public abstract class BubbleGameAPI extends BubblePlugin {
             }
             api.teleportPlayers(api.chosenmap, api.chosen);
             GregorianCalendar now = new GregorianCalendar();
-            now.add(Calendar.SECOND,10);
+            now.add(Calendar.SECOND,5);
             api.timer = new GameTimer(20,now.getTimeInMillis()) {
                 public void run(int seconds) {
                     seconds += 1;
-                    if(seconds <= 3 || seconds % 5 == 0)Messages.broadcastMessageTitle(ChatColor.BLUE + String.valueOf(seconds),ChatColor.AQUA + "The game is starting",new Messages.TitleTiming(5,10,2));
+                    Messages.broadcastMessageTitle(ChatColor.BLUE + String.valueOf(seconds),ChatColor.AQUA + "The game is starting",new Messages.TitleTiming(5,10,2));
                     for(Player p:Bukkit.getOnlinePlayers())p.playSound(p.getLocation().getBlock().getLocation(), Sound.NOTE_BASS,1f,1f);
                 }
 
@@ -341,6 +341,7 @@ public abstract class BubbleGameAPI extends BubblePlugin {
         }
         timer = new GameTimer(20,calendar.getTimeInMillis()) {
             public void run(int seconds) {
+                seconds+= 1;
                 BoardModule module = preset.getModule("StatusValue");
                 for(GameBoard board:GameBoard.getBoards()){
                     BoardScore score = board.getScore(preset,module);
