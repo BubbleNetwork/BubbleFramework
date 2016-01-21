affee78651abfca2f3f853c7f9b72c6a360ab354
ExtendObject <clipcojnh@gmail.com>
2016-01-21 20:40:59 +0000

    Using short instead of byte for Load Priority
    I don't like bytes

--- src/com/thebubblenetwork/api/framework/plugin/LoadPriority.java
+++ src/com/thebubblenetwork/api/framework/plugin/LoadPriority.java
@@ -4,5 +4,5 @@ package com.thebubblenetwork.api.framework.plugin;
  * Created by Jacob on 12/12/2015.
  */
 public class LoadPriority {
-    public static byte LOWEST = 127, HIGHEST = 1, NORMAL = 64, LOW = 96, HIGH = 32;
+    public static short LOWEST = 127, HIGHEST = 1, NORMAL = 64, LOW = 96, HIGH = 32;
 }
